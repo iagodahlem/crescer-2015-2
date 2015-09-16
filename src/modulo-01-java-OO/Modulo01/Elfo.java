02a95e699ddee634bfe1552d132084d925a1c94d
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-16 01:19:18 -0300

    Resolução dos exercícios 'modulo-01-introducao-oo-dia01' do 1 ao 3

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,18 @@
 public class Elfo {
     private String nome;
-    private int flechas;
+    private int flechas = 42;
+    private int experiencia = 0;
     
-    public Elfo(String n, int flech) {
+    public Elfo(String n) {
         nome = n;
-        flechas = flech;
+    }
+    
+    public void atirarFlecha(String alvo) {
+        flechas--;
+        experiencia++;
+    }
+    
+    public void atirarFlechaRefactory() {
+    
     }
 }
