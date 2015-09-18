cba08a3624b50667fec6c41f96c522071bcc1aab
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-18 10:43:05 -0300

    feat(Dwarf): adicione construtor na classe Dwarf

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -10,12 +10,21 @@ public class Dwarf  {
         status = Status.VIVO;
     }
     
-    public void Dwarf(String nome) {
+    public Dwarf(String nome) {
+        this(nome, new DataTerceiraEra(1, 1, 1));
+    }
+    
+    public Dwarf(String nome, DataTerceiraEra dataNascimento) {
         this.nome = nome;
+        this.dataNascimento = dataNascimento;
     }
     
     public void receberFlechada() {
         this.vida -= 10;        
+    }
+    
+    public String getNome() {
+        return this.nome;
     }
     
     public int getVida() {
@@ -27,5 +36,9 @@ public class Dwarf  {
             this.status = Status.MORTO;
         }
         return this.status;
+    }
+    
+    public DataTerceiraEra getDataDeNascimento() {
+        return this.dataNascimento;
     }
 }
