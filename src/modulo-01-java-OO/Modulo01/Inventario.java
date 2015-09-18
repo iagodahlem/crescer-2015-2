2eae165117c9084d56b8722ee9c303ddd3e5f582
Iago Dahlem <iagodahlemlorensini@gmail.com>
2015-09-18 17:35:44 -0300

    feat(Item): adicionar classe Item

---
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -0,0 +1,11 @@
+public class Inventario {
+    private String descricao;
+    private int quantidade;
+    
+    
+    
+    public Inventario(String descricao, int quantidade) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+}
