53efe00e63104cbea16ba990aea39debcf69bdf7
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-17 21:10:25 -0300

    feat(dia): exercicio 4.2

--- src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
@@ -1,6 +1,12 @@
 public class DataTerceiraEra {
     private int dia, mes, ano;
     
+    public DataTerceiraEra(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
     public int getDia() {
         return this.dia;
     }
