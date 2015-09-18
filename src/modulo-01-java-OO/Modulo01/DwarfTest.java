aaa11a07d56f8f23cfdb72f22a1b2d6a75a4dd9e
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-18 10:41:37 -0300

    fix(DwarfTest): correção

--- src/modulo-01-java-OO/Modulo01/DwarfTest.java
+++ src/modulo-01-java-OO/Modulo01/DwarfTest.java
@@ -12,20 +12,20 @@ public class DwarfTest
     
     @Test
     public void dwarfNasceCom110DeVida() {
-        Dwarf gimli = new Dwarf();
+        Dwarf gimli = new Dwarf("Gimli");
         assertEquals(110, gimli.getVida());
     }
     
     @Test
     public void dwarfRecebeFlechadaEPerde10DeVida() {
-        Dwarf gimli = new Dwarf();
+        Dwarf gimli = new Dwarf("Gimli");
         gimli.receberFlechada();
         assertEquals(100, gimli.getVida());
     }
     
     @Test
     public void dwarfRecebe11FlechasEVidaÉ0() {
-        Dwarf gimli = new Dwarf();
+        Dwarf gimli = new Dwarf("Gimli");
         for(int i = 0; i < 11; i++) {
             gimli.receberFlechada();
         }
@@ -34,7 +34,7 @@ public class DwarfTest
     
     @Test
     public void dwarfRecebe7FlechaVezesEVidaÉ40() {
-        Dwarf gimli = new Dwarf();
+        Dwarf gimli = new Dwarf("Gimli");
         for(int i = 0; i < 7; i++) {
             gimli.receberFlechada();
         }
@@ -43,10 +43,26 @@ public class DwarfTest
     
     @Test
     public void dwarfRecebeStatusMorto() {
-        Dwarf gloin = new Dwarf();
+        Dwarf gloin = new Dwarf("Gloin");
         for (int i = 0; i < 11; i++) {
             gloin.receberFlechada();
         }
        assertEquals(Status.MORTO, gloin.getStatus());
     }
+    
+    /*
+    @Test 
+    public void dwarfRecebeNomeEDataDeNascimento() {
+        Dwarf gloin = new Dwarf("Gloin", new DataTerceiraEra(10, 10, 1995));
+        assertEquals("Gloin", gloin.getNome());
+        assertEquals(new DataTerceiraEra(10, 10, 1995), gloin.getDataDeNascimento());
+    }
+    */
+    
+   /*
+    @Test
+    public void dwarfRecebeDataDeNascimentoDefault() {
+    
+    }
+    */
 }
