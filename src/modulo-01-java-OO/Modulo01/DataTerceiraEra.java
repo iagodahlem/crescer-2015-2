a9f308f933a361418f40b64eee9e6ccd3af623b4
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-17 21:05:04 -0300

    feat(dia03): resolucionar exercicios 4.1

---
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
@@ -0,0 +1,15 @@
+public class DataTerceiraEra {
+    private int dia, mes, ano;
+    
+    public int getDia() {
+        return this.dia;
+    }
+    
+    public int getMes() {
+        return this.mes;
+    }
+    
+    public int getAno() {
+        return this.ano;
+    }
+}
