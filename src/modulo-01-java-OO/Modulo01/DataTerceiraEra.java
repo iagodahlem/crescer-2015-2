53c77dd6bd8f9496a976efe4adc36571bc4f19cd
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-19 00:45:56 -0300

    style(DataTerceiraEra): ajustes

--- src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
@@ -20,6 +20,6 @@ public class DataTerceiraEra {
     }
     
     public boolean ehBissexto() {
-        return ((this.ano % 4 == 0) && ((this.ano % 100 != 0) || (this.ano % 400 == 0))) ? true : false;
+        return (this.ano % 4 == 0 && this.ano % 100 != 0) || this.ano % 400 == 0;
     }
 }
