43555a2013aa01205e03e33556edae0bb63c510d
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-19 00:59:23 -0300

    test(DataTerceiraEra): adicionar testes unitarios

--- src/modulo-01-java-OO/Modulo01/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEraTest.java
@@ -5,14 +5,38 @@ import org.junit.Test;
 
 public class DataTerceiraEraTest
 {
-   @Test 
-   public void anoEhBissexto() {
+    @Test
+    public void dataTerceiraEraÉCriadaComDiaMesAno() {
+        DataTerceiraEra primeiroDia = new DataTerceiraEra(1,1,1);
+        assertEquals(1, primeiroDia.getDia());
+        assertEquals(1, primeiroDia.getMes());
+        assertEquals(1, primeiroDia.getAno());
+    }
+    
+    @Test
+    public void dataTerceiraEraÉCriadaComDiaMesAnoNegativo() {
+        DataTerceiraEra primeiroDia = new DataTerceiraEra(-1,-1,-1);
+        assertEquals(-1, primeiroDia.getDia());
+        assertEquals(-1, primeiroDia.getMes());
+        assertEquals(-1, primeiroDia.getAno());
+    }
+    
+    @Test
+    public void dataTerceiraEraÉCriadaComDiaMesAnoZerado() {
+        DataTerceiraEra primeiroDia = new DataTerceiraEra(0,0,0);
+        assertEquals(0, primeiroDia.getDia());
+        assertEquals(0, primeiroDia.getMes());
+        assertEquals(0, primeiroDia.getAno());
+    }
+    
+    @Test 
+    public void anoÉBissexto() {
        DataTerceiraEra bissexto = new DataTerceiraEra(1, 1, 4);
        assertEquals(true, bissexto.ehBissexto());
     }
     
     @Test
-    public void anoNaoEhBissexto() {
+    public void anoNaoÉBissexto() {
         DataTerceiraEra naoBissexto = new DataTerceiraEra(1, 1, 1);
         assertEquals(false, naoBissexto.ehBissexto());
     }
