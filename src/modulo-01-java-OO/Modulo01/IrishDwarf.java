a1b38e3df99d365e7726440a2b97b4ab69c38827
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-22 14:58:13 -0300

    fix(IrishDwarf): corrigir herança construtores

--- src/modulo-01-java-OO/Modulo01/IrishDwarf.java
+++ src/modulo-01-java-OO/Modulo01/IrishDwarf.java
@@ -1,12 +1,15 @@
 public class IrishDwarf extends Dwarf {
     
     public IrishDwarf(){
+        super();
     }
     
     public IrishDwarf(String nome) {
+        super(nome);
     }
     
     public IrishDwarf(String nome, DataTerceiraEra dataNascimento) {
+        super(nome, dataNascimento);
     }
       
     public void tentarSorte() {
