a4f530ab8c17029f7e41d1a7fc7dcf97602c31ca
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-21 23:38:11 -0300

    style(Inventario): corrigir código

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -1,11 +1,7 @@
 import java.util.ArrayList;
 
 public class Inventario {
-    private ArrayList<Item> itens;
-    
-    public Inventario() {
-        this.itens = new ArrayList<Item>();
-    }
+    private ArrayList<Item> itens = new ArrayList<Item>();
     
     public void adicionarItem(Item item) {
         this.itens.add(item);
@@ -16,37 +12,62 @@ public class Inventario {
     }
     
     public ArrayList<Item> getItens() {
-        return itens;
-    }
-    
-    public Item getItem(int posicao){
-        return this.itens.get(posicao);
-    }
-    
-    public int getItensTamanho() {
-        return this.itens.size();
+        return this.itens;
     }
     
     public String getDescricoesItens() {
         String descricao = "";
-        int itens = this.getItensTamanho();
-        for(int i = 0; i < itens; i++) {
-            descricao += this.getItens().get(i).getDescricao();
-            if (itens != 1) {
-                descricao += "," + "";
-            }
+        for (Item item : this.itens) {
+            descricao += item.getDescricao();
         }
-        return descricao;  
+        return descricao.substring(0, descricao.length() - 1);
     }
     
     public Item getItemComMaiorQuantidade() {
-        Item itemComMaiorQuantidade = this.getItens().get(0);
-        int itens = this.getItensTamanho();
-        for(int i = 1; i < itens; i++) {
-            if(itemComMaiorQuantidade.getQuantidade() < this.getItens().get(i).getQuantidade()) {
-                itemComMaiorQuantidade = this.getItens().get(i);
+        int indice = 0, maiorQtd = 0;
+        for (Item item : this.itens) {
+            int qtdAtual = item.getQuantidade();
+            if (qtdAtual > maiorQtd) {
+                indice = this.itens.indexOf(item);
+                maiorQtd = qtdAtual;
             }
         }
-        return itemComMaiorQuantidade;
+        return this.itens.get(indice);
+    }
+    
+    public void ordenarItens() {    
+        // Versão mais simples porém mais instável do BubbleSort - sempre O(n^2)
+        int numeroItens = this.itens.size();
+        
+        for (int i = 0; i < numeroItens; i++) {
+            for (int j = 0; j < numeroItens - 1; j++) {
+                Item itemAtual = this.itens.get(j);
+                Item proximo = this.itens.get(j + 1);
+                
+                boolean precisaTrocar = itemAtual.getQuantidade() > proximo.getQuantidade();
+                
+                if (precisaTrocar) {
+                    this.itens.set(j, proximo);
+                    this.itens.set(j + 1, itemAtual);
+                }
+            }
+        }
+    }
+    
+    public void aumentarQuantidadeEmMil() {
+        for (Item item : this.itens) {
+            item.aumentarQuantidadeEmMil();
+        }
+    }
+    
+    public void aumentarQuantidadeComSomatorio() {
+        for (Item item : this.itens) {
+            item.aumentarQuantidadeComSomatorio();
+        }
+    }
+    
+    public boolean equals(Object obj) {
+        Inventario outroInventario = (Inventario)obj;
+        return this.itens.equals(outroInventario.getItens());
     }
 }   
