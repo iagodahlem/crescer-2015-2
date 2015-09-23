4658913fe7f1d591d5169cf198cbc03ba8d21902
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-23 02:38:00 -0300

    test(OrcSnaga): adicionar teste ao OrcSnaga

---
+++ src/modulo-01-java-OO/Modulo01/OrcSnagaTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrcSnagaTest
+{
+    @Test
+    public void orcSnagaRecebe70() {
+        OrcSnaga snaga = new OrcSnaga();
+        assertEquals(70, snaga.getVida());
+    }
+    
+    @Test
+    public void orcSnagaRecebeInventarioCom1ArcoE5Flechas() {
+        OrcSnaga snaga = new OrcSnaga();
+        Inventario esperado = new Inventario();
+        esperado.adicionarItem(new Item("Arco", 1));
+        esperado.adicionarItem(new Item("Flecha", 5));
+        assertEquals(esperado, snaga.getInventario());
+    }
+}
