b7c87589074b1f3c0436b1566242b7297c980948
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-24 17:18:57 -0300

    style(NossoJogo): alterar nome da classe

---
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -0,0 +1,8 @@
+public class NossoJogo {
+    public final static String NOME = "Bah of the Rings";
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Olá, bem vindo ao " + NOME);
+        }
+    }
+}
