10da70556164eff4d15b1dad7b39e7e49b8486d3
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-24 16:55:37 -0300

    feat(Jogo): adicionar e classe de interface método main

---
+++ src/modulo-01-java-OO/Modulo01/Jogo.java
@@ -0,0 +1,9 @@
+public class Jogo {
+    public final static String NOME = "Bah of the Rings";
+    
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Olá, bem vindo ao " + NOME);
+        }
+    }
+}
