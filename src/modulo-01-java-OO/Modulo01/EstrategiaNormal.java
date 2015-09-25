81cc298f6f008865424e18b92f2a9091e6be5638
iagodahlem <iagodahlemlorensini@gmail.com>
2015-09-25 14:58:21 -0300

    feat(Estrategia): adiocionar Interface de estrategias

---
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class EstrategiaNormal implements EstrategiaDeAtaque{
+    public void atacar(ArrayList<Dwarf> dwarves) {
+        System.out.println("Ataque normal");
+    }
+}
