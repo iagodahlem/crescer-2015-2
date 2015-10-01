c206e95bd5ed64ef157bb80ae561123a581ca355
Iago Dahlem <iagodahlemlorensini@gmail.com>
2015-10-01 17:18:07 -0300

    feat(slide04): versionar resolução dos exercicios slide 4

--- src/modulo-02-banco/exercicios/exercicios-slide-04.sql
+++ src/modulo-02-banco/exercicios/exercicios-slide-04.sql
@@ -35,3 +35,37 @@ From
 	Associado
 Order by	
 	NumeroDeCaracteresDoNome DESC;
+
+-- 6
+-- Faça uma consulta que retorne o nome do associado e a 
+-- data de quando cada completará (ou completou) 50 anos, 
+-- liste também o dia da semana.
+SELECT 
+	Nome, 
+	DATEADD(Year, 50, DataNascimento) as Data50Anos,
+	DATENAME(weekday, DATEADD(Year, 50, DataNascimento)) as DiaSemana 
+FROM 
+	Associado;
+
+-- 7
+-- Liste a quantidade de cidades agrupando por UF.
+SELECT UF, COUNT(1) as QuantidadeDeCidades FROM Cidade GROUP BY UF;
+
+-- 8
+-- Liste as cidades que possuem o mesmo nome e UF.
+SELECT 
+	Nome, UF, COUNT(1) as Total
+FROM 
+	Cidade
+GROUP BY 
+	Nome, UF
+HAVING COUNT(1) > 1;
+
+-- 9
+SELECT 
+	MAX(IDAssociado) + 1 as ProximoID 
+FROM 
+	Associado;
+
+-- 10
+INSERT into CidadeAux Select MIN(IDCidade) as MenorID, Nome, UF FROM Cidade;
