1194396b43a01e065a16d2c9f16aa9b21217d4b8
Iago Dahlem <iagodahlemlorensini@gmail.com>
2015-10-02 13:00:20 -0300

    feat(Exercicios): resolução dos exercicios 1 ao 4 presentes no slide 05

---
+++ src/modulo-02-banco/exercicios/exercicios-slide-05.sql
@@ -0,0 +1,40 @@
+/* Exercicios slide 05 */
+
+-- 1
+Select e.NomeEmpregado as 'Nome do Empregado', 
+	   d.NomeDepartamento as 'Nome do Departamento' 
+From Empregado as e
+INNER JOIN Departamento as d ON d.IDDepartamento = e.IDDepartamento;
+
+-- 2
+Select a.Nome as 'Nome do Associado',
+	   c.Nome as 'Nome da Cidade'
+From Associado as a
+LEFT JOIN Cidade as c ON c.IDCidade = a.IDCidade;
+
+-- 3
+-- Lista os estados (UF) e total de cidades que não possuem associados relacionados 
+-- (dica: pode ser utilizado count + group by + exists).
+
+Select UF as Estados, 
+	   COUNT(Nome) as TotalDeCidades 
+From Cidade 
+Where EXISTS (Select 1 From Associado a Where a.IDCidade = NULL)
+GROUP BY UF;
+
+select * from Associado;
+
+-- 4 
+-- Faça uma consulta que liste o nome do associado, o nome da cidade, 
+-- e uma coluna que indique se a cidade é da região SUL (RS, SC, PR), 
+-- se for imprimir *** (3 asteriscos), senão imprimir nulo.
+
+Select a.Nome as 'Nome do Associado', 
+	   c.Nome as 'Nome do Departamento',
+	   Case when c.UF = 'RS' then '***'
+			when c.UF = 'SC' then '***'
+			when c.UF = 'PR' then '***'
+			else NULL
+	   End Estado
+From Associado as a
+INNER JOIN Cidade as c ON c.IDCidade = a.IDCidade;
