4863e22a0ac2c22672f8261b4a1f3de248d1162a
iagodahlem <iagodahlemlorensini@gmail.com>
2015-10-19 21:00:03 -0200

    feat(exemplo-funtions): versionar arquivo com exemplo de functions

--- src/modulo-03-web/js/exemplos-functions.js
+++ src/modulo-03-web/js/exemplos-functions.js
@@ -6,3 +6,17 @@
 (function(texto){
 	alert(texto);
 })('oi');
+
+throw new Error('');
+
+var arr = ['a', 'b', 'c'];
+for (var prop in arr) {
+	console.log(arr[prop]);
+};
+
+// FOR EACH
+exemplo.forEach() {
+	function(elem, i) {
+		console.log();
+	}
+}
