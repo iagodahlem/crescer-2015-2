fa3d5cd546f5ae2f699761de9bb30dc9edb406fc
iagodahlem <iagodahlemlorensini@gmail.com>
2015-10-19 11:09:24 -0200

    feat(Functions): versionar resolução do exercicio 4 opcional

--- src/modulo-03-web/js/dia-01/lista-js-basico.js
+++ src/modulo-03-web/js/dia-01/lista-js-basico.js
@@ -7,10 +7,10 @@ function dailyGame(num) {
   	if (num % 2 === 0 ) {
     	console.log('Love me not');
 	}
-  	else {
-  		console.log('Love me');
-  	}
+  	console.log('Love me');
 }
+
+/*============================*/
 
 // 2
 function maiorTexto(texto) {
@@ -23,14 +23,44 @@ function maiorTexto(texto) {
   	return maior;
 }
 
+/*============================*/
+
 // 3
 function imprime(nomes, fn) {
-    for (i = 0; i < nomes.length; i++) {
-        var nome = nomes[i];
-        fn(nome);
+    var valida = typeof fn;
+    if (valida === 'function') {
+        for (i = 0; i < nomes.length; i++) {
+            var nome = nomes[i];
+            fn(nome);
+        }
+    }
+    else {
+        console.log('Jabulani: \n');
+        console.log('Type error: paramêtro passado não é uma função.');
     }
 }
 
 function instrutor(instrutor) {
-    console.log('Olá querido instrutor: ', instrutor);
+    console.log('olá querido instrutor: ', instrutor);
+}
+
+/*============================*/
+
+// 4
+function fiboSum(n) {
+    var sum = 1, pen = 0, ult = 1, atual = 0;
+    for (i = 1; i < n; i++) {
+        atual = pen + ult;
+        pen = ult;
+        ult = atual;
+        sum += atual;
+    }
+    return sum;
+}
+
+/*============================*/
+
+// 5
+function excelis(col) {
+
 }
