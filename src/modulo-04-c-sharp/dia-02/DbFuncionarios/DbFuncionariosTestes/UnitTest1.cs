d040faac23d3843340e0183edbbdef1cda6bd73f
iagodahlem <iagodahlemlorensini@gmail.com>
2015-10-28 13:06:22 -0200

    feat(Dia-02): versionar arquivos e resolução dos exercicios do dia 02

---
+++ src/modulo-04-c-sharp/dia-02/DbFuncionarios/DbFuncionariosTestes/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DbFuncionarios;
+using System.Collections.Generic;
+
+namespace DbFuncionariosTestes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BaseDeDadosOrdenaFuncionariosPorCategoria()
+        {
+            var baseDeDados = new BaseDeDados();
+
+            baseDeDados.OrdenadosPorCategoria();
+
+            Assert;
+        }
+    }
+}
