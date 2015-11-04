d52de0fe15bd6a9b12cac20d5d028f30c2849ff0
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-04 05:51:15 -0200

    feat(Locadora): adicionar Project Locadora.MVC

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.MVC/Controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Locadora.Dominio;
+
+namespace Locadora.MVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult ListaJogos()
+        {
+            var baseDeDados = new BaseDeDados();
+            ViewBag.ListaJogos = baseDeDados.ListaJogos;
+            return View("ListaJogos");
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Locadora.MVC.MvcApplication" Language="C#" %>
