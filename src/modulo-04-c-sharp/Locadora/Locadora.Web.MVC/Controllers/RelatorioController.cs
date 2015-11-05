1c218c41d0edd78272c4a00c8d147588fd38d907
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-05 04:14:03 -0200

    chore(Locadora): versionar arquivos do projeto Locadora ja refatorados

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -0,0 +1,36 @@
+﻿using Locadora.Dominio.Repositorio;
+using Locadora.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.Web.MVC.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public IJogoRepositorio repositorio = new Locadora.Repositorio.ADO.JogoRepositorio();
+
+        public ActionResult JogosDisponiveis()
+        {
+            var relatorioModel = new RelatorioModel();
+
+            foreach (var jogo in repositorio.BuscarTodos())
+            {
+                var jogoModel = new JogoModel()
+                {
+                    Id = jogo.Id,
+                    Nome = jogo.Nome,
+                    Preco = jogo.Preco,
+                    Categoria = jogo.Categoria.ToString()
+                };
+                relatorioModel.ListaJogos.Add(jogoModel);
+            }
+
+            relatorioModel.QuantidadeTotal = relatorioModel.ListaJogos.Count();
+
+            return View(relatorioModel);
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<JogoModel> ListaJogos { get; set; }
+        public int QuantidadeTotal { get; set; }
+        public decimal ValorMedio { get; set; }
+        public string JogoMaisCaro { get; set; }
+        public string JogoMaisBarato { get; set; }
+
+        public RelatorioModel()
+        {
+            ListaJogos = new List<JogoModel>();
+        }
+    }
+}
