435034bd2c5fd053cf58ea28698dbef0af880097
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-06 19:59:06 -0200

    fix(JogoDetalhes): ajustar grid da view

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/JogoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.Web.MVC.Controllers
+{
+    public class JogoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
