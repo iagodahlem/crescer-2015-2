5fece70a276ee88ac5e4996fc1e1c53cdfee4857
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-10 00:57:38 -0200

    test(JogoTest): adicionar alguns testes

--- src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/JogoTest.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/JogoTest.cs
@@ -20,11 +20,61 @@ namespace Locadora.Dominio.Test
         [TestMethod]
         public void LocacaoParaClienteTemIdCorreto()
         {
-            Jogo jogo = new Jogo();
+            var jogo = new Jogo();
 
             jogo.LocarPara(new Cliente(id: 1));
 
             Assert.AreEqual(1, jogo.Cliente.Id);
+        }
+
+        [TestMethod]
+        public void JogoRecebeImagemNull()
+        {
+            var jogo = new Jogo(id: 1);
+            string imagemObtida = jogo.URLImagem;
+
+            string resultadoEsperado = null;
+
+            Assert.AreEqual(imagemObtida, resultadoEsperado);
+        }
+
+        [TestMethod]
+        public void JogoRecebeImagemEsperada()
+        {
+            var jogo = new Jogo()
+            {
+                URLImagem = "ibagem do batman"
+            };
+            string imagemObtida = jogo.URLImagem;
+
+            string imagemEsperada = "ibagem do batman";
+
+            Assert.AreEqual(imagemObtida, imagemEsperada);
+        }
+
+        [TestMethod]
+        public void JogoRecebeVideoNull()
+        {
+            var jogo = new Jogo(id: 1);
+            string videoObtido = jogo.URLVideo;
+
+            string resultadoEsperado = null;
+
+            Assert.AreEqual(videoObtido, resultadoEsperado);
+        }
+
+        [TestMethod]
+        public void JogoRecebeVideoEsperada()
+        {
+            var jogo = new Jogo()
+            {
+                URLImagem = "video secret dos iluminati"
+            };
+            string videoObtido = jogo.URLImagem;
+
+            string videoEsperado = "video secret dos iluminati";
+
+            Assert.AreEqual(videoObtido, videoEsperado);
         }
     }
 }
