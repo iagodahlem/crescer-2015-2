b6a270f4f4bbc786f9d4199c799ccc92f26b8e9e
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-10 00:10:24 -0200

    feat(ClienteRepositorio): adicionar ClienteRepositorio com os métodos implementados

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/ClienteRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Locadora.Dominio.Repositorio;
+using System.Collections.Generic;
+using System.Linq;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF
+{
+    public class ClienteRepositorio : RepositorioBase, IClienteRepositorio
+    {
+        public IList<Cliente> BuscarPorNome(string nome)
+        {
+            using (var db = CriarBanco())
+            {
+                return db.Cliente.Where(cliente => cliente.Nome == nome).ToList();
+            }
+        }
+    }
+}
