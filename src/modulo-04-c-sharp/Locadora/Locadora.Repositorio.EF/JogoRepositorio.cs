97ef9d450a22a832628885a6ac1c79f979dd89ce
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-09 23:33:52 -0200

    feat(JogoRepositorio): adicionar class JogoRepositorio

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/JogoRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF
+{
+    public class JogoRepositorio : IJogoRepositorio
+    {
+        public int Atualizar(Jogo jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Jogo BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Jogo> BuscarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Jogo> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Criar(Jogo jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
