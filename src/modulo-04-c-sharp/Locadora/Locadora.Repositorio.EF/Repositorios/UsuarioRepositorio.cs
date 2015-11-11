39dff314bd60fe68ddbd14efeef6eb8053499df8
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-10 23:00:22 -0200

    feat(Repositorios): implementar UsuarioRepositorio

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF.Repositorios
+{
+    public class UsuarioRepositorio : RepositorioBase, IUsuarioRepositorio
+    {
+        public Usuario BuscaPorEmail(string email)
+        {
+            using (var db = CriarBanco())
+            {
+                return db.Usuario.Include("Permissoes").FirstOrDefault(usuario => usuario.Email == email);
+            }
+        }
+    }
+}
