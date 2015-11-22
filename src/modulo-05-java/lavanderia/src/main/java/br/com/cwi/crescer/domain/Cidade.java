b81ef1e09979870e05651dbc0f81f02c282fffd2
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-22 16:39:08 -0200

    feat(domain.Cidade): adicionar getters e setters da entidade Cidade

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/domain/Cidade.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/domain/Cidade.java
@@ -21,4 +21,29 @@ public class Cidade {
 	@Column(name = "UF")
 	@Basic(optional = false)
 	private String uf;
+	
+	public long getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(long idCidade) {
+		this.idCidade = idCidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
 }
