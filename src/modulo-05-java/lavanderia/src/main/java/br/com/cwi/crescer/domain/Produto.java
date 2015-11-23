4d853f6db5e0e307229dcfc29bd2a504d1ec15b7
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-23 00:37:02 -0200

    feat(domain.Produto): adicionar mapeamento da entidade Produto

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/domain/Produto.java
@@ -0,0 +1,66 @@
+package br.com.cwi.crescer.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Produto")
+@SequenceGenerator(name = Produto.SEQUENCE_NAME, sequenceName = Produto.SEQUENCE_NAME)
+public class Produto {
+
+	public static final String SEQUENCE_NAME = "SEQ_Produto";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "IDProduto")
+	private Long idProduto;
+	
+	@Column(name = "IDServico")
+	private Long idServico;
+	
+	@Column(name = "IDMaterial")
+	private Long idMaterial;
+	
+	@Column(name = "Valor")
+	private BigDecimal valor;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public Long getIdServico() {
+		return idServico;
+	}
+
+	public void setIdServico(Long idServico) {
+		this.idServico = idServico;
+	}
+
+	public Long getIdMaterial() {
+		return idMaterial;
+	}
+
+	public void setIdMaterial(Long idMaterial) {
+		this.idMaterial = idMaterial;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+}
