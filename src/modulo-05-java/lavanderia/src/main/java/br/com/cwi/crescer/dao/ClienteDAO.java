5e07734b9ba0774214c29eb155fe114804d55069
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-23 01:00:58 -0200

    feat(DAO.Cliente): adicionar repositorio Cliente

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/dao/ClienteDAO.java
@@ -0,0 +1,24 @@
+package br.com.cwi.crescer.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.cwi.crescer.domain.Cliente;
+
+@Repository
+public class ClienteDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public ClienteDAO(EntityManager em) {
+		super();
+		this.em = em;
+	}
+	
+	public Cliente findById(Long id) {
+		return em.find(Cliente.class, id);
+	}
+}
