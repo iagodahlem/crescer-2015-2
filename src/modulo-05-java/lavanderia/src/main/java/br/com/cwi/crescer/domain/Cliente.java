541bd3fd9b80984b1fce583e1c28682b65a84010
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-23 00:34:44 -0200

    feat(domain.Cliente): adicionar mapeamento da entidade Cliente

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/domain/Cliente.java
@@ -0,0 +1,122 @@
+package br.com.cwi.crescer.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cliente")
+@SequenceGenerator(name = Cliente.SEQUENCE_NAME, sequenceName = Cliente.SEQUENCE_NAME)
+public class Cliente {
+
+	public static final String SEQUENCE_NAME = "SEQ_Cliente";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "Cliente")
+	private Long idPessoa;
+	
+	@Column(name = "Nome", length = 70)
+	@Basic(optional = false)
+	private String nome;
+	
+	@Column(name = "CPF", length = 11)
+	@Basic(optional = false)
+	private String cpf;
+	
+	@Column(name = "Email", length = 100)
+	private String email;
+	
+	@Column(name = "Endereco", length = 50)
+	private String endereco;
+	
+	@Column(name = "Bairro", length = 50)
+	private String bairro;
+
+	@Column(name = "IDCidade")
+	private Long idCidade;
+	
+	@Column(name = "CEP", length = 8)
+	private Long cep;
+	
+	@Column(name = "Situacao", length = 1)
+	private String situacao;
+
+	public Long getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public Long getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(Long idCidade) {
+		this.idCidade = idCidade;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+	
+}
