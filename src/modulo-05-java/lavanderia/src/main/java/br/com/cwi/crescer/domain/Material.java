1cff76fb275bf419af28a35225d175394828ded2
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-23 00:35:52 -0200

    feat(domain.Material): adicionar mapeamento da entidade Material

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/domain/Material.java
@@ -0,0 +1,44 @@
+package br.com.cwi.crescer.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Material")
+@SequenceGenerator(name = Material.SEQUENCE_NAME, sequenceName = Material.SEQUENCE_NAME)
+public class Material {
+	
+	public static final String SEQUENCE_NAME = "SEQ_Material";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "IDMaterial")
+	private Long idMaterial;
+	
+	@Column(name = "Descricao", length = 30)
+	@Basic(optional = false)
+	private String descricao;
+	
+	public Long getIdMaterial() {
+		return idMaterial;
+	}
+	
+	public void setIdMaterial(Long idMaterial) {
+		this.idMaterial = idMaterial;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
