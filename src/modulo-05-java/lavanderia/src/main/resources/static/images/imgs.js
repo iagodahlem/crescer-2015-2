825bcd63409b8e6e7f6fa53b2ef56d8cb0d4b0b9
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-24 03:26:44 -0200

    feat(layout): adicionar layout para a aplicação

---
+++ src/modulo-05-java/lavanderia/src/main/resources/static/images/imgs.js
@@ -0,0 +1,5 @@
+var img1 = 'https://static.pexels.com/photos/8999/night.jpg';
+var img2 = 'https://static.pexels.com/photos/10646/photo-1439546743462-802cabef8e97.jpeg';
+var img3 = 'https://static.pexels.com/photos/8883/pexels-photo.jpeg';
+var img4 = 'https://static.pexels.com/photos/9691/pexels-photo.jpeg';
+var img5 = 'https://static.pexels.com/photos/17739/pexels-photo.jpg';
