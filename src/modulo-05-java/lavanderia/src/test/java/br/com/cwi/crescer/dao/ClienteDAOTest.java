17d0e17b7906828091e69930fbbc49a3ec2abe76
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-24 00:24:01 -0200

    feat(ClienteDAO): adicionar método para buscar por Situacao

--- src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/dao/ClienteDAOTest.java
+++ src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/dao/ClienteDAOTest.java
@@ -1,11 +1,14 @@
 package br.com.cwi.crescer.dao;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.cwi.crescer.AbstractInfrastructureTest;
 import br.com.cwi.crescer.domain.Cliente;
+import br.com.cwi.crescer.domain.Cliente.SituacaoCliente;
 
 public class ClienteDAOTest extends AbstractInfrastructureTest {
 
@@ -17,6 +20,17 @@ public class ClienteDAOTest extends AbstractInfrastructureTest {
 		Cliente cliente = clienteDAO.findById(1L);
 		Assert.assertNotNull(cliente);
 	}
+	
+	@Test
+    public void deveBuscarClientesAtivos() throws Exception {
+        List<Cliente> clientes = clienteDAO.findBySituacao(SituacaoCliente.ATIVO);
+        Assert.assertNotNull(clientes);
+        Assert.assertFalse(clientes.isEmpty());
+
+        for (Cliente cliente : clientes) {
+            Assert.assertEquals(SituacaoCliente.ATIVO, cliente.getSituacao());
+        }
+    }
 	
 	@Test
 	public void deveBuscarClienteComPedidos() throws Exception {
