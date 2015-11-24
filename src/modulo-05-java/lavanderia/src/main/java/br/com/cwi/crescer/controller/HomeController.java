5651aaa8cfa19464a2709b31bb6b783e7d103420
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-24 00:36:37 -0200

    style(HomeController): renomear controller para melhor legibilidade

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/HomeController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class HomeController {
 
 	@RequestMapping({ "/", "/index", "/home"})
 	public String index(Model model) {
