4e4f892ebcd9906dc4936f3b6098d309185bce29
iagodahlem <iagodahlemlorensini@gmail.com>
2015-11-24 21:17:35 -0200

    refactor(HomeController): remover parametro model

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/HomeController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/HomeController.java
@@ -1,14 +1,13 @@
 package br.com.cwi.crescer.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping({ "/", "/index", "/home"})
-	public String index(Model model) {
+	public String index() {
 		return "index";
 	}
 }
